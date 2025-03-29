5c6fdf1c0a2b2948d8324c66a2693725418a7b8a
fadel benomar <fadel.benomar3241@gmail.com>
2025-03-29 10:57:07 +0000

    suppression utilis li khassra

--- krabs_manage/src/Controller/Admin/HoraireCrudController.php
+++ krabs_manage/src/Controller/Admin/HoraireCrudController.php
@@ -7,6 +7,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use App\Enum\JourEnum;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
 
 class HoraireCrudController extends AbstractCrudController
 {
@@ -14,7 +19,22 @@ class HoraireCrudController extends AbstractCrudController
     {
         return Horaire::class;
     }
-
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+    
+            ChoiceField::new('jour')
+                ->setChoices(JourEnum::cases())
+                ->formatValue(fn ($value) => $value?->name)
+                ->renderAsNativeWidget(),
+    
+            TimeField::new('heureOuverture'),
+            TimeField::new('heureFermeture'),
+            AssociationField::new('enseigne'),
+        ];
+    }
+    
     /*
     public function configureFields(string $pageName): iterable
     {
